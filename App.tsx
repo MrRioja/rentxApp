3d7b91be9fc7bf94d0a580993a5ec4cba10054a8
Luiz Rioja <lulyrioja@gmail.com>
2021-09-10 22:59:48 -0300

    :alien: Loading fonts into the app and entering the theme provider

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
+import { Home } from "./src/screens/Home";
+import theme from "./src/styles/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <ThemeProvider theme={theme}>
+        <AppLoading />
+      </ThemeProvider>
+    );
+  }
+
+  return <Home />;
+}
