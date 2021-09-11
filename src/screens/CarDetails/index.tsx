6fd11e937515793fdc321025bd7c0f090cb9b508
Luiz Rioja <lulyrioja@gmail.com>
2021-09-11 20:52:15 -0300

    :alien: Implementing and styling CarDetails component

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+import { Accessory } from "../../components/Accessory";
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
+import { Button } from "../../components/Button";
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3XlZJBBD6cxFxgDOo0T8vJUHRViFXeG_4IA&usqp=CAU",
+          ]}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Accessories>
+
+        <About>
+          Porro sit modi magnam. Modi magnam dolorem ut eius sit. Quiquia ipsum
+          numquam quisquam. Etincidunt dolorem ipsum adipisci voluptatem labore.
+          Voluptatem dolor consectetur ut dolore consectetur velit etincidunt.
+          Sed adipisci dolore quiquia. Porro est dolorem aliquam.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
