6754c688f0955f66def74dc7c48fdfc5b4b01b19
Luiz Rioja <lulyrioja@gmail.com>
2021-09-13 22:12:51 -0300

    :alien: Fixing type of routes in navigate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,18 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { StackRoutes } from "./stack.routes";
 
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: string;
+      CarDetails: string;
+      Scheduling: string;
+      SchedulingDetails: string;
+      SchedulingComplete: string;
+    }
+  }
+}
+
 export function Routes() {
   return (
     <NavigationContainer>
