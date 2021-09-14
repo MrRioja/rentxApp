6f7890dbf1bcb50a5fdcc089bc3612ff45b6482f
Luiz Rioja <lulyrioja@gmail.com>
2021-09-14 00:26:36 -0300

    :alien: Making car information dynamic in the CarDetails component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+import { CarDTO } from "../../dtos/CarDTO";
 
 import { Button } from "../../components/Button";
 import { Accessory } from "../../components/Accessory";
@@ -30,8 +32,14 @@ import {
   Footer,
 } from "./styles";
 
+interface Params {
+  car: CarDTO;
+}
+
 export function CarDetails() {
   const navigation = useNavigation();
+  const route = useRoute();
+  const { car } = route.params as Params;
 
   function handleConfirmRental() {
     navigation.navigate("Scheduling");
@@ -44,41 +52,33 @@ export function CarDetails() {
       </Header>
 
       <CarImages>
-        <ImageSlider
-          imagesUrl={[
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3XlZJBBD6cxFxgDOo0T8vJUHRViFXeG_4IA&usqp=CAU",
-          ]}
-        />
+        <ImageSlider imagesUrl={car.photos} />
       </CarImages>
 
       <Content>
         <Details>
           <Description>
-            <Brand>Lamborghini</Brand>
-            <Name>Huracan</Name>
+            <Brand>{car.brand}</Brand>
+            <Name>{car.name}</Name>
           </Description>
 
           <Rent>
-            <Period>Ao dia</Period>
-            <Price>R$ 580</Price>
+            <Period>{car.rent.period}</Period>
+            <Price>R$ {car.rent.price}</Price>
           </Rent>
         </Details>
 
         <Accessories>
-          <Accessory name="380Km/h" icon={speedSvg} />
-          <Accessory name="3.2s" icon={accelerationSvg} />
-          <Accessory name="800 HP" icon={forceSvg} />
-          <Accessory name="Gasolina" icon={gasolineSvg} />
-          <Accessory name="Auto" icon={exchangeSvg} />
-          <Accessory name="2 pessoas" icon={peopleSvg} />
+          {car.accessories.map((accessory) => (
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={speedSvg}
+            />
+          ))}
         </Accessories>
 
-        <About>
-          Porro sit modi magnam. Modi magnam dolorem ut eius sit. Quiquia ipsum
-          numquam quisquam. Etincidunt dolorem ipsum adipisci voluptatem labore.
-          Voluptatem dolor consectetur ut dolore consectetur velit etincidunt.
-          Sed adipisci dolore quiquia. Porro est dolorem aliquam.
-        </About>
+        <About>{car.about}</About>
       </Content>
 
       <Footer>
