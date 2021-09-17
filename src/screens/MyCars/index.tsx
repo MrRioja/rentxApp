cbf344c62165b56a05817822f27c31115a814d29
Luiz Rioja <lulyrioja@gmail.com>
2021-09-16 23:59:05 -0300

    :alien: Starting the implementation and styling of the MyCars screen

---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { CarDTO } from "../../dtos/CarDTO";
+import { api } from "../../services/api";
+
+import { Container } from "./styles";
+
+export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get("/schedules_byuser?user_id=1");
+
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
+
+  return <Container></Container>;
+}
