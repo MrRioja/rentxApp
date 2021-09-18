0a8e7a8d4cf80508c109bf71e051ae0a18c13a8b
Luiz Rioja <lulyrioja@gmail.com>
2021-09-17 23:00:46 -0300

    :alien: Finalizing the implementation and styling of the MyCars screen

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,12 +1,46 @@
 import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "styled-components";
+import { FlatList, StatusBar } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
 import { CarDTO } from "../../dtos/CarDTO";
 import { api } from "../../services/api";
 
-import { Container } from "./styles";
+import { BackButton } from "../../components/BackButton";
+import { Load } from "../../components/Load";
+import { Car } from "../../components/Car";
+
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
+} from "./styles";
+
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
+}
 
 export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const navigation = useNavigation();
+  const theme = useTheme();
 
   useEffect(() => {
     async function fetchCars() {
@@ -24,5 +58,63 @@ export function MyCars() {
     fetchCars();
   }, []);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        <BackButton
+          onPress={() => navigation.goBack()}
+          color={theme.colors.shape}
+        />
+
+        <Title>
+          Escolha uma{"\n"}
+          data de inicio e {"\n"}
+          fim do aluguel
+        </Title>
+
+        <SubTitle>Conforto, segurança e praticidade</SubTitle>
+      </Header>
+
+      {isLoading ? (
+        <Load />
+      ) : (
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
+
+          <FlatList
+            data={cars}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <CarWrapper>
+                <Car data={item.car} />
+                <CarFooter>
+                  <CarFooterTitle>Período</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign
+                      name={"arrowright"}
+                      size={20}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWrapper>
+            )}
+            showsVerticalScrollIndicator={false}
+          />
+        </Content>
+      )}
+    </Container>
+  );
 }
