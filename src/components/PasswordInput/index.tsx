5bf69d10f35aed16503262376d40052ac784b027
Luiz Rioja <lulyrioja@gmail.com>
2021-09-26 23:56:48 -0300

    :alien: Creating and styling the passwordInput component

---
+++ src/components/PasswordInput/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components";
+
+import { Container, IconContainer, InputText } from "./styles";
+import { BorderlessButton } from "react-native-gesture-handler";
+
+interface Props extends TextInputProps {
+  iconName: React.ComponentProps<typeof Feather>["name"];
+}
+
+export function PasswordInput({ iconName, ...rest }: Props) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+  const theme = useTheme();
+
+  function handlePasswordVisibilityChange() {
+    setIsPasswordVisible((prevState) => !prevState);
+  }
+
+  return (
+    <Container>
+      <IconContainer>
+        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      </IconContainer>
+      <InputText {...rest} secureTextEntry={isPasswordVisible} />
+
+      <BorderlessButton onPress={handlePasswordVisibilityChange}>
+        <IconContainer>
+          <Feather
+            name={isPasswordVisible ? "eye" : "eye-off"}
+            size={24}
+            color={theme.colors.text_detail}
+          />
+        </IconContainer>
+      </BorderlessButton>
+    </Container>
+  );
+}
---
+++ src/components/PasswordInput/styles.ts
@@ -0,0 +1,24 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex-direction: row;
+`;
+
+export const IconContainer = styled.View`
+  height: 56px;
+  width: 55px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 2px;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+export const InputText = styled.TextInput`
+  flex: 1;
+  padding: 0 23px;
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
