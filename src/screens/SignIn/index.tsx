f2703c06b7987dee2625fe13e681a7de86693dc5
Luiz Rioja <lulyrioja@gmail.com>
2021-09-26 23:57:16 -0300

    :alien: Creating and Styling the SignIn Screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import theme from "../../styles/theme";
+
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
+
+import { Container, Header, Title, SubTitle, Form, Footer } from "./styles";
+
+export function SignIn() {
+  return (
+    <Container>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Title>Estamos{"\n"}quase lá.</Title>
+        <SubTitle>
+          Faça seu login para começar{"\n"}uma experiência incrível.
+        </SubTitle>
+      </Header>
+
+      <Form>
+        <Input
+          iconName="mail"
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+
+        <PasswordInput iconName="lock" placeholder="Senha" />
+      </Form>
+
+      <Footer>
+        <Button
+          title="Login"
+          onPress={() => {}}
+          enabled={false}
+          loading={false}
+        />
+
+        <Button
+          title="Criar conta gratuita"
+          onPress={() => {}}
+          enabled={false}
+          loading={false}
+          light
+          color={theme.colors.background_secondary}
+        />
+      </Footer>
+    </Container>
+  );
+}
