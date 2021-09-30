72cc43c25e6accb9fa86efe0794083d5a786a767
Luiz Rioja <lulyrioja@gmail.com>
2021-09-29 23:04:26 -0300

    :alien: Validating login data and entering button functions

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,7 +4,10 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
+import * as Yup from "yup";
+
 import theme from "../../styles/theme";
 
 import { Button } from "../../components/Button";
@@ -12,10 +15,40 @@ import { Input } from "../../components/Input";
 import { PasswordInput } from "../../components/PasswordInput";
 
 import { Container, Header, Title, SubTitle, Form, Footer } from "./styles";
+import { useNavigation } from "@react-navigation/core";
 
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigation = useNavigation();
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+
+        password: Yup.string().required("A senha é obrigatória"),
+      });
+
+      await schema.validate({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Opa", error.message);
+      } else {
+        Alert.alert(
+          "Erro na autenticação",
+          "Ocorreu um erro ao fazer login, verifique as credencias"
+        );
+      }
+    }
+  }
+
+  function handleNewAccount() {
+    navigation.navigate("SignUpFirstStep");
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -55,15 +88,15 @@ export function SignIn() {
           <Footer>
             <Button
               title="Login"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
             />
 
             <Button
               title="Criar conta gratuita"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleNewAccount}
+              enabled={true}
               loading={false}
               light
               color={theme.colors.background_secondary}
