343d08fe0e0ae09ab01fee2e7d8919d2998dad24
Luiz Rioja <lulyrioja@gmail.com>
2021-09-30 00:12:43 -0300

    :alien: Making Confirmation Screen Content Dynamic

--- src/screens/Confirmation/index.tsx
+++ src/screens/Confirmation/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { StatusBar, useWindowDimensions } from "react-native";
 
 import LogoSvg from "../../assets/logo_background_gray.svg";
@@ -9,13 +9,21 @@ import { ConfirmButton } from "../../components/ConfirmButton";
 
 import { Container, Content, Title, Message, Footer } from "./styles";
 
-export function SchedulingComplete() {
+interface Params {
+  title: string;
+  message: string;
+  nextScreenRoute: string;
+}
+
+export function Confirmation() {
   const { width } = useWindowDimensions();
 
   const navigation = useNavigation();
+  const route = useRoute();
+  const { title, message, nextScreenRoute } = route.params as Params;
 
   function handleConfirm() {
-    navigation.navigate("Home");
+    navigation.navigate(nextScreenRoute);
   }
 
   return (
@@ -29,13 +37,8 @@ export function SchedulingComplete() {
 
       <Content>
         <DoneSvg width={80} height={80} />
-        <Title>Carro alugado!</Title>
-
-        <Message>
-          Agora você só precisa ir {"\n"}
-          até a concessionária da RENTX {"\n"}
-          pegar o seu automóvel.
-        </Message>
+        <Title>{title}</Title>
+        <Message>{message}</Message>
       </Content>
 
       <Footer>
