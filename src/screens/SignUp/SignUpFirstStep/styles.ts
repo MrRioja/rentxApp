e4ba6cfe9286ac9c49d5f7e4a0535fbc93ab3a9a
Luiz Rioja <lulyrioja@gmail.com>
2021-09-29 23:02:32 -0300

    :alien: Creating the first SignUp step screen

---
+++ src/screens/SignUp/SignUpFirstStep/styles.ts
@@ -0,0 +1,49 @@
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  padding: 0 24px;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: ${getStatusBarHeight() + 31}px;
+`;
+
+export const Steps = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(40)}px;
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  margin-top: 60px;
+  margin-bottom: 16px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: ${RFValue(15)}px;
+  line-height: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+`;
+
+export const Form = styled.View`
+  width: 100%;
+  margin-top: 64px;
+  margin-bottom: 16px;
+`;
+
+export const FormTitle = styled.Text`
+  margin-bottom: 24px;
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+`;
