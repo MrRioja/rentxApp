81e840f58d548db14e0dc96696168d1d73d6d517
Luiz Rioja <lulyrioja@gmail.com>
2021-10-02 23:59:14 -0300

    :alien: Fixing checking if the user is logged in to render the correct routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -24,7 +24,7 @@ export function Routes() {
 
   return (
     <NavigationContainer>
-      {user ? <AppTabRoutes /> : <AuthRoutes />}
+      {user.id ? <AppTabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
