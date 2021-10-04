7ee5069e8412f7c8661bc77626a4ee46db4b7cf4
Luiz Rioja <lulyrioja@gmail.com>
2021-10-03 23:45:42 -0300

    :alien: Creating and styling profile screen content

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { useTheme } from "styled-components";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 
+import * as ImagePicker from "expo-image-picker";
+
+import { useTheme } from "styled-components";
+import { useAuth } from "../../hooks/auth";
+import { useNavigation } from "@react-navigation/native";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+
+import { Input } from "../../components/Input";
 import { BackButton } from "../../components/BackButton";
+import { PasswordInput } from "../../components/PasswordInput";
 
 import {
   Container,
@@ -14,35 +26,120 @@ import {
   PhotoContainer,
   Photo,
   PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
+  Section,
 } from "./styles";
 
 export function Profile() {
+  const { user, signOut } = useAuth();
+
+  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicense, setDriverLicense] = useState(user.driver_license);
+
   const theme = useTheme();
   const navigation = useNavigation();
 
-  function handleSignOut() {}
+  function handleOptionChange(optionSelected: "dataEdit" | "passwordEdit") {
+    setOption(optionSelected);
+  }
+
+  async function handleAvatarSelect() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (result.cancelled) {
+      return;
+    }
+
+    if (result.uri) {
+      setAvatar(result.uri);
+    }
+  }
 
   return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton
-            color={theme.colors.shape}
-            onPress={() => navigation.goBack()}
-          />
-          <HeaderTitle>Editar Perfil</HeaderTitle>
-          <LogoutButton onPress={handleSignOut}>
-            <Feather name="power" size={24} color={theme.colors.shape} />
-          </LogoutButton>
-        </HeaderTop>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <HeaderTop>
+              <BackButton
+                color={theme.colors.shape}
+                onPress={() => navigation.goBack()}
+              />
+              <HeaderTitle>Editar Perfil</HeaderTitle>
+              <LogoutButton onPress={signOut}>
+                <Feather name="power" size={24} color={theme.colors.shape} />
+              </LogoutButton>
+            </HeaderTop>
 
-        <PhotoContainer>
-          <Photo source={{ uri: "https://github.com/mrrioja.png" }} />
-          <PhotoButton onPress={() => {}}>
-            <Feather name="camera" size={24} color={theme.colors.shape} />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
-    </Container>
+            <PhotoContainer>
+              {!!avatar && <Photo source={{ uri: avatar }} />}
+              <PhotoButton onPress={handleAvatarSelect}>
+                <Feather name="camera" size={24} color={theme.colors.shape} />
+              </PhotoButton>
+            </PhotoContainer>
+          </Header>
+
+          <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+            <Options>
+              <Option
+                active={option === "dataEdit"}
+                onPress={() => handleOptionChange("dataEdit")}
+              >
+                <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
+              </Option>
+
+              <Option
+                active={option === "passwordEdit"}
+                onPress={() => handleOptionChange("passwordEdit")}
+              >
+                <OptionTitle active={option === "passwordEdit"}>
+                  Trocar senha
+                </OptionTitle>
+              </Option>
+            </Options>
+
+            {option === "dataEdit" ? (
+              <Section>
+                <Input
+                  iconName="user"
+                  placeholder="Nome"
+                  autoCorrect={false}
+                  onChangeText={setName}
+                  defaultValue={user.name}
+                />
+                <Input
+                  iconName="mail"
+                  editable={false}
+                  defaultValue={user.email}
+                />
+                <Input
+                  iconName="credit-card"
+                  placeholder="CNH"
+                  keyboardType="numeric"
+                  onChangeText={setDriverLicense}
+                  defaultValue={user.driver_license}
+                />
+              </Section>
+            ) : (
+              <Section>
+                <PasswordInput iconName="lock" placeholder="Senha atual" />
+                <PasswordInput iconName="lock" placeholder="Nova senha" />
+                <PasswordInput iconName="lock" placeholder="Repetir senha" />
+              </Section>
+            )}
+          </Content>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
