a4749eac2fb437e5dea18b377d766736a1da0ae1
Luiz Rioja <lulyrioja@gmail.com>
2021-10-11 01:14:16 -0300

    :alien: Waiting for authentication hook to return before rendering routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useAuth } from "../hooks/auth";
 import { AppTabRoutes } from "./app.tab.routes";
 import { AuthRoutes } from "./auth.routes";
+import { LoadAnimation } from "../components/LoadAnimation";
 
 declare global {
   namespace ReactNavigation {
@@ -20,9 +21,11 @@ declare global {
 }
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  return (
+  return loading ? (
+    <LoadAnimation />
+  ) : (
     <NavigationContainer>
       {user.id ? <AppTabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
