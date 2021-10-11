081319b4695a1e5d4bf587724af6098513bad24e
Luiz Rioja <lulyrioja@gmail.com>
2021-10-11 01:13:33 -0300

    :alien: Creating loading in the authentication hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -29,6 +29,7 @@ interface AuthContextData {
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>;
   updatedUser: (user: User) => Promise<void>;
+  loading: boolean;
 }
 
 interface AuthProviderProps {
@@ -39,6 +40,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [data, setData] = useState<User>({} as User);
+  const [loading, setLoading] = useState(true);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -116,6 +118,7 @@ function AuthProvider({ children }: AuthProviderProps) {
         api.defaults.headers.authorization = `Bearer ${userData.token}`;
 
         setData(userData);
+        setLoading(false);
       }
     }
 
@@ -123,7 +126,9 @@ function AuthProvider({ children }: AuthProviderProps) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data, signIn, signOut, updatedUser }}>
+    <AuthContext.Provider
+      value={{ user: data, signIn, signOut, updatedUser, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
