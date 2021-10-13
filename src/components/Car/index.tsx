670bdb542cb91695a7ae34e253388d3702ed4a5f
Luiz Rioja <lulyrioja@gmail.com>
2021-10-12 21:31:09 -0300

    :alien: Hiding the car price if the user is offline

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RectButtonProps } from "react-native-gesture-handler";
 import { getAccessoryIcon } from "../../utils/getAccessoryIcon";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 import { Car as ModelCar } from "../../database/model/Car";
 
@@ -22,6 +23,7 @@ interface Props extends RectButtonProps {
 }
 
 export function Car({ data, ...rest }: Props) {
+  const netInfo = useNetInfo();
   const MotorIcon = getAccessoryIcon(data.fuel_type);
 
   return (
@@ -33,7 +35,7 @@ export function Car({ data, ...rest }: Props) {
         <About>
           <Rent>
             <Period>{data.period}</Period>
-            <Price>R$ {data.price}</Price>
+            <Price>{`R$ ${netInfo.isConnected ? data.price : "..."}`}</Price>
           </Rent>
 
           <Type>
